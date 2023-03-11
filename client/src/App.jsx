1ba68f08659509fb467539e298f412a4aa8a338a
SOV3333 <8282076+sov3333@users.noreply.github.com>
2023-03-11 17:34:36 +0800

    install chakra, implement navbar and buttons

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
 
 import { logo, logo_full } from './assets';
 import { Home, CreatePost, UpdatePost } from './pages';
+import { SidebarWithHeader } from './components';
 import { Button } from '@chakra-ui/react';
 import { AddIcon } from '@chakra-ui/icons';
 
@@ -10,26 +11,32 @@ import './App.css'
 const App = () => {
   return (
     <BrowserRouter>
-      <header>
-        <Link to='/'>
-          <img src={logo} alt="logo" />
-        </Link>
-        <Link to='/create'>
-          <Button colorScheme='green' variant='solid' leftIcon={<AddIcon />}>
-            Create a Post
-          </Button>
-        </Link>
-      </header>
-      <main>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/create" element={<CreatePost />} />
-          <Route path="/update/:id" element={<UpdatePost />} />
-        </Routes>
-      </main>
-      <footer>
-        <img src={logo_full} alt="logo" />
-      </footer>
+      <SidebarWithHeader children={(
+        <>
+        <header>
+          <Link to='/'>
+            <img src={logo} alt="logo" />
+          </Link>
+          <Link to='/create'>
+            <Button colorScheme='green' variant='solid' leftIcon={<AddIcon />}>
+              Create a Post
+            </Button>
+          </Link>
+        </header>
+        <main>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/create" element={<CreatePost />} />
+            <Route path="/update/:id" element={<UpdatePost />} />
+          </Routes>
+        </main>
+        <footer>
+          <img src={logo_full} alt="logo" />
+        </footer>
+        </>
+      )} />
+        
+      
     </BrowserRouter>
   )
 }
